6c465b3ea9d6a7a610493aef11e40973de6a8e32
ayakje <121613138+ayakje@users.noreply.github.com>
2022-12-29 14:55:57 +0200

    Initial commit

---
+++ Ipear/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Ipear.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Ipear.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            Picks = GetPicks();
+        }
+        public List<Pick> Picks { get; set; }
+
+        public ICommand OrderCommand => new Command(() => Application.Current.MainPage.Navigation.PushAsync(new OrderPage()));
+        private List<Pick> GetPicks()
+        {
+            return new List<Pick>
+            {
+                new Pick { Title = "Iphone 14 Black Pink", Image = "phone5.png", Description = "discount - 1300 $ " },
+                new Pick { Title = "Iphone 14 Gold", Image = "phone4.png", Description = "discount - 1200 $ " }
+            };
+        }
+
+    }
+
+
+    public class Pick
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
---
+++ Ipear/ViewModel/OrderViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Ipear.ViewModel
+{
+    public class OrderViewModel : BaseViewModel
+    {
+        public OrderViewModel()
+        {
+            MenuList = GetMenu();
+        }
+        public List<Pick> MenuList { get; set; }
+
+        public ICommand BackCommand => new Command(() => Application.Current.MainPage.Navigation.PopAsync());
+
+        private List<Pick> GetMenu()
+        {
+            return new List<Pick>
+            {
+                new Pick { Title = "IPhone 11 pink", Image = "phone1.png", Description = "128 Gb ", Price = "$560" },
+                new Pick { Title = "IPhone 12 red", Image = "phone2.png", Description = "256 Gb", Price = "$720" },
+                new Pick { Title = "IPhone 12 blue", Image = "phone3.png", Description = "64 Gb", Price = "$650" },
+                new Pick { Title = "Iphone 14 Gold", Image = "phone4.png", Description = "512 Gb", Price = "$1200" },
+                new Pick { Title = "IPhone 14 Black Pinck", Image = "phone5.png", Description = "1 Tb", Price = "$1300" }
+            };
+        }
+    }
+}
